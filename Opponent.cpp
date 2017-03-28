9a4370bbbcc729cba6bf540eb17d2a03e4b3d143
DrGFreeman <jul.dlbt@gmail.com>
2017-03-27 21:01:19 -0400

    Added Opponent class

---
+++ Opponent.cpp
@@ -0,0 +1,98 @@
+#include <Opponent.h>
+
+// Window size for sensors median filter
+const byte irMFSize = 7;
+
+Opponent::Opponent(unsigned int maxDist, const byte mfSize) :
+  _sensL(A0, mfSize), _sensR(A2, mfSize)
+{
+  _maxDist = maxDist;
+
+  // Set Sharp sensors parameters
+  _sensL.setModel(_sensL.GP2Y0A60SZLF_5V);
+  _sensR.setModel(_sensR.GP2Y0A60SZLF_5V);
+  _sensL.setValMinMax(134, 875);
+  _sensR.setValMinMax(134, 875);
+}
+
+// Check for opponent and update state variables
+void Opponent::check()
+{
+  // Reset state variable
+  _inSight = false;
+
+  // Get distance from sensors
+  unsigned int distL = _sensL.getDist();
+  unsigned int distR = _sensR.getDist();
+
+  // Check if opponent is visible
+  if (distL < _maxDist)
+  {
+    // Opponent is visible to the left sensor
+    _inSight = true;
+  }
+  else
+  {
+    // Opponent is not visible to the left sensor
+    distL = _maxDist;
+  }
+  if (distR < _maxDist)
+  {
+    // Opponent is visible to the right sensor
+    _inSight = true;
+  }
+  else
+  {
+    // Opponent is not visible to the right sensor
+    distR = _maxDist;
+  }
+
+  // Determine opponent position; positive = to the left
+  _position = distR - distL;
+
+  if (_inSight)
+  {
+    // Note current time
+    _timeLastSeen = millis();
+
+    if (_position > 0)
+    {
+      _direction = DIR_LEFT;
+    }
+    else
+    {
+      _direction = DIR_RIGHT;
+    }
+  }
+}
+
+// Return opponent direction
+byte Opponent::direction()
+{
+  return _direction;
+}
+
+// Return true if opponent is in sight
+bool Opponent::isInSight()
+{
+  if (_inSight)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+// Return the position of the opponent
+int Opponent::position()
+{
+  return _position;
+}
+
+// Return time when opponent was last seen (in ms)
+unsigned long Opponent::timeLastSeen()
+{
+  return _timeLastSeen;
+}
---
+++ Opponent.h
@@ -0,0 +1,61 @@
+#ifndef Opponent_h
+#define Opponent_h
+
+#include <Arduino.h>
+#include <SharpDistSensor.h>
+
+class Opponent
+{
+public:
+  enum directions
+  {
+    DIR_LEFT,
+    DIR_RIGHT
+  };
+
+  /* Constructor
+   * maxDist is the max distance to look for the opponent
+   * mfSize is the window size for the sensors median filter
+  */
+  Opponent(unsigned int maxDist, const byte mfSize);
+
+  // Check for opponent and update state variables
+  void check();
+
+  // Return opponent direction
+  byte direction();
+
+  // Return true if opponent is in sight
+  bool isInSight();
+
+  /* Return the position of the opponent
+   * The position is defined as the difference between the right and left sensor
+   * distances. A positive number means the opponent is to the left.
+  */
+  int position();
+
+  // Return time when opponent was last seen (in ms)
+  unsigned long timeLastSeen();
+
+private:
+  // Opponent direction
+  byte _direction;
+
+  // Opponent is in sight
+  bool _inSight;
+
+  // Last time opponent was seen
+  unsigned long _timeLastSeen;
+
+  // Max distance to look for opponent
+  unsigned int _maxDist;
+
+  // Opponent position
+  int _position;
+
+  // Sharp distance sensors
+  SharpDistSensor _sensL;
+  SharpDistSensor _sensR;
+};
+
+#endif
